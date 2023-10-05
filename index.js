1e546f7c08210e3912afdf035c448c3aad4038a6
tiagolimar <tiagolima.r2@gmail.com>
2023-10-05 19:03:38 -0300

    criando endpoint

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ const host = 'localhost';
 const port = 3333;
 
 server.get('/',(request,response)=>{
-    return "Página inicial"
+    return response.send("Página de produto")
+})
+server.get('/produtos',(request,response)=>{
+    return response.send("Página de produtos - GET")
+})
+server.get('/produto/:id',(request,response)=>{
+    const {id} = request.params;
+    return response.send("Página de produto id:" + id +"- GET")
+})
+server.post('/produto',(request,response)=>{
+    return response.send("Página de produto - POST")
+})
+server.put('/produto/:id',(request,response)=>{
+    return response.send("Página de produto - PUT")
+})
+server.delete('/produto/:id',(request,response)=>{
+    return response.send("Página de produto - DELETE")
 })
 
 
